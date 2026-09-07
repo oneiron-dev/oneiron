52e5a79a2f68d0d7887d3b9a00ea3d7e4f90f6db
Lexi <olety7@gmail.com>
2026-09-07 15:54:05 +0900

    ONE-1832: separate base batch application from session pipeline

--- crates/oneiron/src/batch/mod.rs
+++ crates/oneiron/src/batch/mod.rs
@@ -3,6 +3,7 @@ pub(crate) mod secret_scan;
 
 mod agent_definition_create;
 mod authority_log;
+mod base_apply;
 mod builder;
 mod child_of_overlay;
 mod claim_candidate_apply;
@@ -31,6 +32,7 @@ pub use self::txn_builder::TxnBatchBuilder;
 pub(crate) use self::vad_postcommit::VadPostcommitScope;
 
 pub(crate) use self::authority_log::validate_replicated_authority_log_for_local_vault;
+pub(crate) use self::base_apply::{apply_ops_with_origin, apply_session_bundle_claim_puts};
 pub(crate) use self::builder::BatchOp;
 pub(crate) use self::child_of_overlay::child_of_prefix;
 pub(crate) use self::claim_materialization::{ClaimMaterialization, apply_owner_bound_claim_puts};
@@ -49,8 +51,8 @@ pub(crate) use self::gate_mode::ApplyOpsGateMode;
 pub(crate) use self::gate_staging::StagedClaimGateOutcome;
 use self::gate_staging::{stage_preflight_decision, staged_claim_gate_outcomes};
 pub(crate) use self::ops_pipeline::{
-    BaseWriteOrigin, apply_ops, apply_ops_session, apply_ops_with_gate_mode, apply_ops_with_origin,
-    apply_session_bundle_claim_puts, reject_overlay_member_base_write,
+    BaseWriteOrigin, apply_ops, apply_ops_session, apply_ops_with_gate_mode,
+    reject_overlay_member_base_write,
 };
 pub(crate) use self::phonetic_apply::delete_from_phonetic_postings;
 pub(crate) use self::put_apply::delete_entity_index_rows;
